728442100e69613095f8533321620a87478352fd
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2021-10-17 19:22:36 +0900

    스크립트추가

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 var CaptureBtn = document.querySelector("#recordVideo");
-CaptureBtn.addEventListener("click",selectArea);
+CaptureBtn.addEventListener("click", function (e) {
+  document.querySelector("body").classList.add("edit_cursor");
+  selectArea(e);
+});
 
-function selectArea()
-{
-  
+function selectArea() {
+  window.onload
 }
