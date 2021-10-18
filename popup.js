12640e9b34f4cc7dc7bcabb173040684621c2100
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2021-10-18 19:52:21 +0900

    addDimmed

--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
+var height = window.innerHeight;
+var $mask = $('<div id="screenshot_mask"></div>').css("border-width", "0 0 " + height + "px 0");
+$("body").append($mask);
+
+
 var CaptureBtn = document.querySelector("#recordVideo");
 CaptureBtn.addEventListener("click", function (e) {
   document.querySelector("body").classList.add("edit_cursor");
@@ -5,5 +10,6 @@ CaptureBtn.addEventListener("click", function (e) {
 });
 
 function selectArea() {
-  window.onload
+
+  //window.onload
 }
